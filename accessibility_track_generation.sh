61f8d13d240fbbbe6b06cd0ad70496262c7e7fea
suhas-rao <suhasrao@post.harvard.edu>
2022-01-06 00:15:35 -0800

    Update accessibility_track_generation.sh

--- accessibility_track_generation.sh
+++ accessibility_track_generation.sh
@@ -15,7 +15,7 @@ threads=24
 chr="chr1|chr2|chr3|chr4|chr5|chr6|chr7|chr8|chr9|chr10|chr11|chr12|chr13|chr14|chr15|chr16|chr17|chr18|chr19|chr20|chr21|chr22|chrX"
 outFile="output.bw"
 
-while getopts "g:t:c:h" opt; do
+while getopts "g:t:c:ho:" opt; do
     case $opt in
         g) chromSizes=$OPTARG ;;
         h) printHelpAndExit 0;;
